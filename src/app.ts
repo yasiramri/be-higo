5522bbc46e573b61532878f76fb2f8205b399f9c
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-07-25 21:18:09 +0700

    initial server port 4999 and installing dependencies

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import path from 'path';
+import createRouter from 'express-file-routing';
+
+const main = async () => {
+  dotenv.config;
+
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json);
+  app.use(morgan('dev'));
+
+  const router = express.Router();
+
+  await createRouter(router, {
+    directory: path.join(__dirname, 'routes'),
+  });
+
+  app.use('/api', router);
+
+  app.listen(4999, () => {
+    console.log('Server started on port 4999');
+  });
+};
+main();
