c4add1a44b5069f349575ff5721da1e1e6848820
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-07-26 12:35:48 +0700

    delete dist

--- dist/utils/db1.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db1 = void 0;
-const mongo_db_1 = require("../../generated/mongo_db");
-exports.db1 = new mongo_db_1.PrismaClient();
